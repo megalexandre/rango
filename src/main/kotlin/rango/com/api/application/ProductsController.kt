66e9bf7ddbd12dbd56f14b0e60903dd490f999e0
megalexandre <megalexandre@gmail.com>
2025-03-16 22:36:37 -0300

    custom metrics

--- src/main/kotlin/rango/com/api/application/ProductsController.kt
+++ src/main/kotlin/rango/com/api/application/ProductsController.kt
@@ -1,5 +1,7 @@
 package rango.com.api.application
 
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
@@ -13,10 +15,14 @@ import java.net.URI
 @RestController
 @RequestMapping("/products")
 class ProductsController(
-    private val productService: ProductService
+    private val productService: ProductService,
+    private val meterRegistry: MeterRegistry
 ) {
 
     private val log: Logger = LoggerFactory.getLogger(this::class.java)
+
+    private var counter: Counter = Counter.builder("created_product").
+        register(meterRegistry);
 
     @GetMapping
     fun getProducts() = productService.getAllProducts().also { log.info("getting all products") }
@@ -27,6 +33,7 @@ class ProductsController(
 
         val response = productService.save(request.toEntity()).toResponse()
         log.info("created product $response")
+        counter.increment()
 
         return ResponseEntity.created(URI.create("/products")).body(response)
     }
